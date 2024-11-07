3ba104b1cec6741499eef8c82180a51d7d32afc8
Danny <danny.f.gomez@gmail.com>
2024-11-07 10:55:35 -0500

    add visual feedback when goal item pressed

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,13 +1,18 @@
 import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 function GoalItem(props) {
-
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
-        <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+    <View>
+      <Pressable
+        android_ripple={{ color: 'darkpurple' }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <View style={styles.goalItem}>
+          <Text style={styles.goalText}>{props.text}</Text>
+        </View>
+      </Pressable>
+    </View>
   );
 }
 
@@ -17,13 +22,16 @@ const styles = StyleSheet.create({
   goalItem: {
     borderRadius: 10,
     backgroundColor: 'purple',
-    padding: 8,
     margin: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
   goalText: {
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 20,
+    padding: 8,
   },
 });
